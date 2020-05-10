1c400ad36272512767dccd302a899418b3bb8b0d
Mateusz <makusmierczyk@gmail.com>
2020-05-10 23:18:16 +0200

    start files added

---
+++ development/js/app.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import {BrowserRouter, HashRouter, Link, Route} from "react-router-dom";
+
+
+
+const App = () => {
+    return (
+        <>
+            <div> Hello JSX</div>
+            </>
+
+)
+}
+
+ReactDOM.render(<App/>, document.getElementById("app"));

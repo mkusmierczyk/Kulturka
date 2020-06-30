5eb47c16bdf75ab8a764b0b37da766069d330e25
Mateusz <makusmierczyk@gmail.com>
2020-06-30 19:02:08 +0200

    beta version v1.18

--- development/js/auth/signOut.js
+++ development/js/auth/signOut.js
@@ -4,10 +4,13 @@ import {AuthContext} from "./auth";
 
 export const SignOut = () => {
     const { currentUser } = useContext(AuthContext);
+
+    const user = currentUser.email.substring(0, currentUser.email.lastIndexOf("@"))
+  
     return (
         <>
             <div className="signOut row">
-                <span className=" signOut__name col-10">Witaj {currentUser.email}<button className="signOut__name__btn" onClick={ () => app.auth().signOut() }> Wyloguj</button></span>
+                <span className=" signOut__name col-10">Witaj {user}<button className="signOut__name__btn" onClick={ () => app.auth().signOut() }> Wyloguj</button></span>
             </div>
         </>
     )
